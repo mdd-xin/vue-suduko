acc6ca5147425937274be21bf869066ceceac361
newMdd <3011878963@qq.com>
2022-09-30 22:40:01 +0800

    feat:增加主题切换功能

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <div class="wrap">
-    <div
-      class="content_wrap"
-      v-for="(item,x) in arr"
-      :key="x"
-    >
-      <div
-        :class="{'block':'true','target_block':block.value==0}"
-        v-for="(block,y) in item"
-        :id="'item-'+x+y"
-        @click="moveBlock(block)"
-        :key="y"
-      >
+  <div id="title">suduko</div>
+  <div class="wrap theme-light" ref="wrap">
+    <div class="content_wrap" v-for="(item,x) in arr" :key="x">
+      <div :class="{'block':'true','target_block':block.value==0}" v-for="(block,y) in item" :id="'item-'+x+y"
+        @click="moveBlock(block)" :key="y">
         <!-- {{x}}{{y}} -->
         {{block.value}}
       </div>
     </div>
   </div>
+  <switch-theme @cutTheme='cutTheme' :curTheme='theme'/>
 </template>
 
 <script setup>
-import { reactive, watch, onMounted, watchEffect } from 'vue'
+import {ref,reactive, watch, onMounted, watchEffect } from 'vue';
+import switchTheme from './components/switchTheme.vue';
 class Block {
   constructor(value, x, y) {
     this.value = value;
@@ -28,7 +22,7 @@ class Block {
     this.y = y;
     this.el = document.querySelector(`#item-${this.x}${this.y}`);
   }
-  shake () {
+  shake() {
     this.el.classList.add('shake');
     setTimeout(() => {
       this.el.classList.remove('shake');
@@ -40,7 +34,7 @@ let arr = reactive([
   [4, 5, 6],
   [7, 0, 8],
 ])
-
+// let result=ref('你会玩不啊~');
 let targetCoord = reactive({})
 
 onMounted(() => {
@@ -58,13 +52,13 @@ onMounted(() => {
   })
 })
 
-function exchange (s, t) {
+function exchange(s, t) {
   let num = arr[s.x][s.y].value;
   arr[s.x][s.y].value = arr[t.x][t.y].value;
   arr[t.x][t.y].value = num;
 }
 
-function moveBlock (block) {
+function moveBlock(block) {
   if (block.x == targetCoord.x) {
     if (block.y == targetCoord.y + 1 || block.y == targetCoord.y - 1) {
       exchange(block, targetCoord)
@@ -83,6 +77,16 @@ function moveBlock (block) {
     block.shake();
   }
 }
+let wrap=ref(null);
+let theme=ref('light')
+function cutTheme(color){
+  theme.value=color;
+  if(theme.value=='light'){
+    wrap.value.classList.replace('theme-dark','theme-light');
+  }else{
+    wrap.value.classList.replace('theme-light','theme-dark');
+  }
+}
 
 watch(arr, () => {
   let str = ''
@@ -95,20 +99,31 @@ watch(arr, () => {
 </script>
 
 <style>
-* {
+  * {
   margin: 0;
   padding: 0;
 }
+</style>
+<style lang="scss" scoped>
 .wrap {
   height: 300px;
   width: 300px;
-  background-color: aqua;
+  background-color: dark;
   padding: 10px;
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   margin: 10px auto;
 }
+// switch theme
+.theme-light{
+  background-color: rgb(119, 123, 168);
+}
+
+.theme-dark{
+  background-color: rgb(44,54,79);
+}
+
 .content_wrap {
   flex: 1 1 auto;
   width: 100%;
@@ -116,9 +131,11 @@ watch(arr, () => {
   justify-content: space-around;
   align-items: center;
 }
+
 .content_wrap:nth-child(2) {
   margin: 10px 0;
 }
+
 .block {
   font-size: 3em;
   flex: 1 1 auto;
@@ -129,6 +146,7 @@ watch(arr, () => {
   align-items: center;
   cursor: pointer;
 }
+
 .block:nth-child(2) {
   margin: 0 10px;
 }
@@ -141,23 +159,34 @@ watch(arr, () => {
   animation: shake 800ms ease-in-out;
 }
 
+#title {
+  width: 100%;
+  text-align: center;
+  margin: 15px 0;
+}
+
 @keyframes shake {
+
   10%,
   90% {
     transform: translate3d(-1px, 0, 0);
   }
+
   20%,
   80% {
     transform: translate3d(+2px, 0, 0);
   }
+
   30%,
   70% {
     transform: translate3d(-4px, 0, 0);
   }
+
   40%,
   60% {
     transform: translate3d(+4px, 0, 0);
   }
+
   50% {
     transform: translate3d(-4px, 0, 0);
   }
---
+++ src/components/switchTheme.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="theme-wrap">
+        <div class="theme-item light" @click="switchTheme" ref="theme"></div>
+        <div class="theme-item github" @click="toGithub"></div>
+    </div>
+</template>
+
+<script setup>
+import {ref,defineEmits,defineProps} from 'vue'
+// let themeColor=ref('orange');
+let theme=ref();
+const emit=defineEmits(['cutTheme']);
+const props=defineProps({
+    curTheme:{
+        type:String
+    }
+})
+console.log(props);
+function switchTheme(){
+    if(props.curTheme=='light'){
+        theme.value.classList.replace('light','dark');
+        emit('cutTheme','dark');
+    }else{
+        theme.value.classList.replace('dark','light');
+        emit('cutTheme','light');
+    }
+}
+function toGithub(){
+    window.open('https://github.com/mdd-xin/vue-suduko.git')
+}
+</script>
+
+<style lang="scss" scoped>
+:root{
+    color-scheme: dark;
+}
+#theme-wrap {
+    height: 12vh;
+    width: 8%;
+    margin: 10px auto;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    // transition: width .3s ease-in-out;
+    position: relative;
+
+    .theme-item {
+        height: 30px;
+        width: 30px;
+        border-radius: 50%;
+        cursor: pointer;
+        border: 1px solid black;
+    }
+
+    // &:hover {
+    //     width: 10%;
+    // }
+
+    .light {
+        background:no-repeat center/100% url('../assets/img/light.png');
+    }
+    .dark{
+        background:no-repeat center/80% url('../assets/img/dark.png');
+    }
+    .github{
+        background:no-repeat center/100% url('../assets/img/github-fill.png');
+    }
+}
+</style>
